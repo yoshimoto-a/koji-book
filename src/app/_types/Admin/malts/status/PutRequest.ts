efaf6a858f5264bed2b31cda04e7e1bf4911bb7d
rs-akane <rs.akane.y@gmail.com>
2025-01-24 13:12:03 +0900

    管理者用

---
+++ src/app/_types/Admin/malts/status/PutRequest.ts
@@ -0,0 +1,5 @@
+import { MaltRole } from "@prisma/client";
+
+export type PutRequest = {
+  maltRole: MaltRole;
+};
---
+++ src/app/api/admin/malts/[id]/route.ts
@@ -0,0 +1,35 @@
+import { buildPrisma } from "@/app/_utils/prisma";
+import { NextRequest, NextResponse } from "next/server";
+import { getCurrentUser } from "@/app/api/_utils/getCurrentUser";
+import { PutRequest } from "@/app/_types/Admin/malts/status/PutRequest";
+interface Props {
+  params: Promise<{
+    id: string;
+  }>;
+}
+export const PUT = async (request: NextRequest, { params }: Props) => {
+  const prisma = await buildPrisma();
+  try {
+    const user = await getCurrentUser({ request });
+    if (user.role !== "ADMIN") {
+      throw new Error("権限がありません");
+    }
+    const { id } = await params;
+    const { maltRole }: PutRequest = await request.json();
+    await prisma.maltArticle.update({
+      where: {
+        id,
+      },
+      data: {
+        status: "PUBLIC",
+        maltRole,
+      },
+    });
+
+    return NextResponse.json({ message: "success!" }, { status: 200 });
+  } catch (e) {
+    if (e instanceof Error) {
+      return NextResponse.json({ error: e.message }, { status: 400 });
+    }
+  }
+};
