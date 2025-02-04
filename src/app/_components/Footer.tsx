bb51564d4d973b29ce0d2cc94ae6fd6897f3ed5f
rs-akane <rs.akane.y@gmail.com>
2025-02-04 22:52:45 +0900

    スタイル調整

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer: React.FC = () => {
   return (
-    <footer className="bg-light_beige w-full h-8 z-10 flex justify-center items-center">
+    <footer className="bg-light_beige w-full h-8 z-10 flex justify-center items-center mt-auto">
       <p className="text-xs">
         &copy; {new Date().getFullYear()} koji-book. All rights reserved.
       </p>
