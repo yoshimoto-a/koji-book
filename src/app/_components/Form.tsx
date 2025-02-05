9625cf64eb46f4ba912952eb167fb9b5f7df5dcc
rs-akane <rs.akane.y@gmail.com>
2025-02-05 14:13:04 +0900

    ログイン情報をローカルストレージに追加

--- src/app/_components/Form.tsx
+++ src/app/_components/Form.tsx
@@ -8,8 +8,8 @@ interface Props {
 
 export const Form: React.FC<Props> = ({ children, onSubmit, title }) => {
   return (
-    <div className="flex items-center justify-center flex-col min-h-screen">
-      <h2 className="text-2xl pt-10 pb-5">{title}</h2>
+    <div className="flex items-center justify-center flex-col h-[calc(100vh-102px)]">
+      <h2 className="text-2xl pb-5">{title}</h2>
       <form
         onSubmit={onSubmit}
         className="mx-2 px-10 py-20 border-2 rounded-lg"
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,33 +3,35 @@ import { Form } from "../_components/Form";
 import { Input } from "@/app/_components/Input";
 import { Button } from "@/app/_components/Button";
 import { useLogin } from "./_hooks/useLogin";
-
 export default function Page() {
   const { register, handleSubmit, errors, isSubmitting } = useLogin();
+
   return (
     <Form onSubmit={handleSubmit} title="ログイン">
-      <Input
-        label="メールアドレス"
-        disabled={isSubmitting}
-        id="email"
-        inputMode="email"
-        placeholder="メールアドレス"
-        type="email"
-        errors={errors.email}
-        register={register("email")}
-      />
-      <Input
-        label="パスワード"
-        disabled={isSubmitting}
-        id="password"
-        inputMode="text"
-        placeholder="パスワード"
-        type="password"
-        errors={errors.password}
-        register={register("password")}
-      />
-      <div className="mt-4 h-9">
-        <Button type="submit">ログイン</Button>
+      <div className="flex flex-col gap-3">
+        <Input
+          label="メールアドレス"
+          disabled={isSubmitting}
+          id="email"
+          inputMode="email"
+          placeholder="メールアドレス"
+          type="email"
+          errors={errors.email}
+          register={register("email")}
+        />
+        <Input
+          label="パスワード"
+          disabled={isSubmitting}
+          id="password"
+          inputMode="text"
+          placeholder="パスワード"
+          type="password"
+          errors={errors.password}
+          register={register("password")}
+        />
+        <div className="mt-4 h-9">
+          <Button type="submit">ログイン</Button>
+        </div>
       </div>
     </Form>
   );
