2999b3eca3ed4d0f8d81e82513c7834701255304
rs-akane <rs.akane.y@gmail.com>
2025-02-10 14:17:03 +0900

    SSR→SWRに変更

--- src/app/recipes/[id]/edit/page.tsx
+++ src/app/recipes/[id]/edit/page.tsx
@@ -1,17 +1,15 @@
-import { IndexResponse } from "@/app/_types/Recipe/IndexResponse";
+"use client";
 import { ArticleForm } from "./_components/ArticleForm";
-interface Props {
-  params: Promise<{
-    id: string;
-  }>;
-}
-export default async function EditPage({ params }: Props) {
-  const { id } = await params;
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_APP_BASE_URL}/api/recipes/${id}`
-  );
-  const data: IndexResponse = await response.json();
+import { useRecipe } from "../_hooks/useRecipe";
+import { useParams } from "next/navigation";
 
+export default function EditPage() {
+  const { id } = useParams();
+  const { data, error } = useRecipe({ id: id as string });
+
+  if (!data) return <div className="text-center pt-10">読込み中...</div>;
+  if (error)
+    return <div className="text-center pt-10">エラー が発生しました</div>;
   return (
     <div className="max-w-md mx-auto py-10 px-5">
       <h2 className="text-2xl">編集する</h2>
