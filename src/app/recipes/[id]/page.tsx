c48551be5bd35fa1c5102a0cd589cb2e7b3f2553
rs-akane <rs.akane.y@gmail.com>
2025-02-11 01:16:32 +0900

    SSG→SSRに修正

--- src/app/recipes/[id]/page.tsx
+++ src/app/recipes/[id]/page.tsx
@@ -10,14 +10,15 @@ interface Props {
 export default async function Page({ params }: Props) {
   const { id } = await params;
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_APP_BASE_URL}/api/recipes/${id}`
+    `${process.env.NEXT_PUBLIC_APP_BASE_URL}/api/recipes/${id}`,
+    { cache: "no-cache" }
   );
   const data: IndexResponse = await response.json();
 
   return (
     <div className="max-w-md mx-auto py-10 px-5">
       <div className="flex justify-end pb-5"></div>
-      <RecipeContent initialValue={data} id={id} />
+      <RecipeContent initialValue={data} />
     </div>
   );
 }
