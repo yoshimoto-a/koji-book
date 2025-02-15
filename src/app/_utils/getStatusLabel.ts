df3c8d70daa186c68f660eed9bb50e3ae8c792ad
rs-akane <rs.akane.y@gmail.com>
2025-02-15 14:58:38 +0900

    enum status

--- src/app/_utils/getStatusLabel.ts
+++ src/app/_utils/getStatusLabel.ts
@@ -2,11 +2,11 @@ import { Status } from "@prisma/client";
 type StatusLabels = "下書き" | "申請中" | "公開";
 export const getStatusLabel = (status: Status): StatusLabels => {
   switch (status) {
-    case "DRAFT":
+    case Status.DRAFT:
       return "下書き";
-    case "PENDING_APPROVAL":
+    case Status.PENDING_APPROVAL:
       return "申請中";
-    case "PUBLIC":
+    case Status.PUBLIC:
       return "公開";
   }
 };
