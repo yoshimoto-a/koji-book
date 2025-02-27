34bd5c7aedbbe181346fc401455151d7c87e20ee
rs-akane <rs.akane.y@gmail.com>
2025-02-27 13:57:06 +0900

    fix useFetch params

--- src/app/_components/AdminArea.tsx
+++ src/app/_components/AdminArea.tsx
@@ -3,8 +3,9 @@ import { useUser } from "../_hooks/useUser";
 import { CurrentlyApplying } from "./CurrentlyApplying";
 export const AdminArea: React.FC = () => {
   const { data, error } = useUser();
-  if (!data) return <div className="text-center">読込み中...</div>;
-  if (error) return <div>エラー が発生しました</div>;
-  if (data.user?.role !== "ADMIN") return null;
+  if (!data) return null;
+  if (error) return null;
+  if (!data.user) return null;
+  if (data.user.role !== "ADMIN") return null;
   return <CurrentlyApplying />;
 };
---
+++ src/app/_components/UserActions.tsx
@@ -0,0 +1,61 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGear,
+  faUser,
+  faArrowRightFromBracket,
+} from "@fortawesome/free-solid-svg-icons";
+import { Unread } from "./Unread";
+import { useSupabaseSession } from "../_hooks/useSupabaseSession";
+import Link from "next/link";
+import { supabase } from "../_utils/supabase";
+import toast from "react-hot-toast";
+
+export const UserActions: React.FC = () => {
+  const { session } = useSupabaseSession();
+  const logout = async () => {
+    try {
+      const { error } = await supabase.auth.signOut();
+      if (error) {
+        throw new Error(error.message);
+      }
+      toast.success("ログアウトしました");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  if (!session) return null;
+
+  return (
+    <div className="flex justify-start items-center gap-3">
+      {session.user.role === "ADMIN" && (
+        <Link href={"/admin"} className="flex flex-col gap-1 items-center">
+          <FontAwesomeIcon
+            icon={faGear}
+            className="text-dark_brown text-2xl mt-1"
+          />
+          <div className="text-xs">管理画面</div>
+        </Link>
+      )}
+      <Unread />
+      <Link href={"/mypage"} className="flex flex-col gap-1 items-center">
+        <FontAwesomeIcon
+          icon={faUser}
+          className="text-dark_brown text-2xl mt-1"
+        />
+        <div className="text-xs">マイページ</div>
+      </Link>
+      <button
+        onClick={logout}
+        type="button"
+        className="flex flex-col gap-1 items-center justify-center"
+      >
+        <FontAwesomeIcon
+          icon={faArrowRightFromBracket}
+          className="text-dark_brown text-2xl mt-1"
+        />
+        <div className="text-xs">ログアウト</div>
+      </button>
+    </div>
+  );
+};
